c4752de744355e051859e72010147726c04bd686
chaechae <iky1505@naver.com>
2023-05-10 16:49:14 +0900

    문제 수정, 등록 서비스구현 제외 완료

--- Algo/src/main/java/com/Seoul5/Algo/model/dao/ProblemDao.java
+++ Algo/src/main/java/com/Seoul5/Algo/model/dao/ProblemDao.java
@@ -3,6 +3,7 @@ package com.Seoul5.Algo.model.dao;
 import java.util.List;
 
 import com.Seoul5.Algo.model.dto.Problem;
+import com.Seoul5.Algo.model.dto.ProblemInfo;
 import com.Seoul5.Algo.model.dto.SearchCondition;
 
 public interface ProblemDao {
@@ -16,5 +17,7 @@ public interface ProblemDao {
 	List<Problem> selectAll();
 	
 	List<Problem> selectByCondition(SearchCondition searchCondition);
+	
+	int insertProblemInfo(ProblemInfo problemInfo); //해당하는 알고리즘 넣는 메서드
 
 }
--- Algo/src/main/java/com/Seoul5/Algo/model/service/ProblemServiceImpl.java
+++ Algo/src/main/java/com/Seoul5/Algo/model/service/ProblemServiceImpl.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.Seoul5.Algo.model.dao.ProblemDao;
 import com.Seoul5.Algo.model.dto.Problem;
+import com.Seoul5.Algo.model.dto.ProblemInfo;
 import com.Seoul5.Algo.model.dto.SearchCondition;
 
 @Service
@@ -16,33 +16,39 @@ public class ProblemServiceImpl implements ProblemService {
 
 	@Autowired
 	private ProblemDao pDao;
-
+	  
 	@Override
 	@Transactional
-	public int insertProblem(Problem problem) {
+	public int insertProblem(Problem problem) { //문제 넣는 메서드
 		return pDao.insertProblem(problem);
 	}
 
 	@Override
 	@Transactional
-	public int updateProblem(Problem problem) {
+	public int updateProblem(Problem problem) { //문제 평점 업데이트 메서드
+		
 		return pDao.updateProblem(problem);
 	}
 
 	@Override
-	public Problem searchByNum(int pNum) {
+	public Problem searchByNum(int pNum) { //문제번호로 문제찾기 메서드
 		return pDao.searchByNum(pNum);
 	}
 	
 	@Override
-	public List<Problem> selectAll() {
+	public List<Problem> selectAll() { //모든 문제리스트정보 반환하는 메서드
 		return pDao.selectAll();
 	}
-	
 	 
 	@Override
-	public List<Problem> selectByCondition(SearchCondition searchCondition) {
+	public List<Problem> selectByCondition(SearchCondition searchCondition) { //검색조건으로 문제리스트 반환하는 메서드
 		return pDao.selectByCondition(searchCondition);
 	}
+
+	@Override 
+	public int insertProblemInfo(ProblemInfo problemInfo) { //해당하는 알고리즘 넣는 메서드
+		return pDao.insertProblemInfo(problemInfo);
+	}
+	 
 
 }
